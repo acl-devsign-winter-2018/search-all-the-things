c335554a578706818c700d513aedb91242063890
Charlie Heiner <heinercj@gmail.com>
2018-02-22 07:52:28 -0800

    moved search components out of app.js into own search folder

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import { search } from '../services/starWarsApi.js';
-import Search from './search';
-import SwList from './swList';
-import Dropdown from './dropdown';
+import SearchAll from './search/SearchAll';
+
 
 
 
@@ -16,50 +14,18 @@ export default class App extends Component {
   };
 
 
-  searchSWAPI = () => {
-    this.setState({
-      results: null,
-      loading: true,
-      error: null,
-    });
   
-
-    const { topic } = this.state; 
-
-    search(topic).then(
-      ({ results }) => 
-      { this.setState({ results });
-      },
-      error => {
-        this.setState({ error, results: null });
-      })
-      .then(() => {
-        this.setState({ loading: false });
-      });
-    
-  }; 
-  
-  handleSearch = topic => {
-    this.setState({ topic }, this.searchSWAPI);
-  };
 
 
   render(){
-    const { results, loading } = this.state;
+   
     return (
       <div>
+
         <header>
-          <Search onSearch={this.handleSearch}/>
+          <SearchAll/>
         </header>
-        <div>
-          <Dropdown/>
-        </div>
-        <div className="loading">{loading && 'Loading...'}</div>
-      
-        <div>
-          {results && <SwList results={results}/>}
-        </div>
-          
+
       </div>
 
     );
