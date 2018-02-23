86b9bc02e3b280abcc4552c57e9da91a1d67eddd
Christina Mills <chrimills@gmail.com>
2018-02-23 10:16:25 -0800

    sucessfully updating title through search and api call

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -19,7 +19,7 @@ export default class App extends Component {
         search(this.state.topic)
             .then(response => {
                 this.setState({
-                    title: response.title
+                    title: response.Title
                 })
             })
 
