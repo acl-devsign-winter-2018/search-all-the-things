3efffe40b61e8c05c83e0951dfa45dd120f0f739
Charlie Heiner <heinercj@gmail.com>
2018-02-22 22:15:53 -0800

    moved api call part of detail into detail container.

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -6,30 +6,30 @@ import { Link } from 'react-router-dom';
 
 export default class Details extends Component {
     
-    static propTypes = {
-      name: PropTypes.string.isRequired
-    };
+  static propTypes = {
+    name: PropTypes.string.isRequired
+  };
 
-    state = {
-      searchDetail: null
-    };
+   state = {
+     searchDetail: null
+   };
 
-    componentDidMount(){
-      search(this.props.name)
-        .then(searchDetail => this.setState({ searchDetail }));
-    }
+  // componentDidMount(){
+  //   search(this.props.name)
+  //     .then(searchDetail => this.setState({ searchDetail }));
+  // }
 
-    render() {
-      const { searchDetail } = this.state;
+   render() {
+     const { searchDetail } = this.state;
 
-      if(name === null) return null;
+     if(searchDetail === null) return null;
 
-      return (
-        <div>
-          <Link to="/search">Search</Link>
-          <pre>{JSON.stringify(searchDetail, true, 2)}</pre>
-        </div>
-      );
-    }
+     return (
+       <div>
+         <Link to="/search">Search</Link>
+         <pre>{JSON.stringify(searchDetail, true, 2)}</pre>
+       </div>
+     );
+   }
 
 }
