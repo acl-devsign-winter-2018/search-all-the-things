6590ced8b4c95bf8bcd7d976287bf8aa5e04ccc4
Christina Mills <chrimills@gmail.com>
2018-02-26 12:08:12 -0800

    fixed typo breaking build

---
+++ src/services/apiResponse.test.js
@@ -0,0 +1,3 @@
+import { checkResponseData } from './movieAPI';
+
+
--- src/services/movieAPi.js
+++ src/services/movieAPi.js
@@ -6,15 +6,14 @@ export const checkResponseData = data => {
     if(data.Response === 'True') return data;
     throw data.Error;
   };
+  
+export function getMovie(id) {
+    const url = `${BASE_URL}s=${topic}&page=${page}&apikey=${API_KEY}&i=${id}`;
+    return get(url);
+}
 
 export function search(topic, page) {
     const url = `${BASE_URL}s=${topic}&page=${page}&apikey=${API_KEY}`;
     console.log(url)
     return fetch(url).then(res => res.json());
-}
-
-export function getMovie(id) {
-    const url = `${BASE_URL}s=${topic}&page=${page}&apikey=${API_KEY}&i=${id}`;
-    return get(url);
-
-}
+};
